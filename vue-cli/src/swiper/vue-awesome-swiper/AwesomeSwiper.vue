adf1e0b444d3f852dfbac745240b766164b434e9
weiyajun2688 <weiyajun2688@ipalfish.com>
2019-05-28 19:42:36 +0800

    canvas

--- vue-cli/src/swiper/vue-awesome-swiper/AwesomeSwiper.vue
+++ vue-cli/src/swiper/vue-awesome-swiper/AwesomeSwiper.vue
@@ -1,14 +1,18 @@
 <template>
   <div id='swiper'>
     <p>swiper demo</p>
-    <swiper ref='swiper' class='swiper-style' :options="swiperOption" @slideChangeTransitionStart='handleChange'>
+    <swiper ref='swiper'
+      class='swiper-style'
+      :options="swiperOption"
+      @slideChangeTransitionStart='handleChange'>
       <swiper-slide>Slide 1</swiper-slide>
       <swiper-slide>Slide 2</swiper-slide>
       <swiper-slide>Slide 3</swiper-slide>
       <swiper-slide>Slide 4</swiper-slide>
       <swiper-slide>Slide 5</swiper-slide>
       <swiper-slide>Slide 6</swiper-slide>
-      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-pagination"
+        slot="pagination"></div>
     </swiper>
   </div>
 </template>
@@ -19,9 +23,12 @@ import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
 export default {
   name: 'swiper-demo',
-  data: function() {
+  data: function () {
     return {
       swiperOption: {
+        spaceBetween: 30,
+        slidesPerView: 'auto',
+        centeredSlides: true,
         pagination: {
           el: '.swiper-pagination'
         },
@@ -36,11 +43,11 @@ export default {
   compouted: {},
   watch: {},
   methods: {
-    handleChange: function() {
+    handleChange: function () {
       console.log(this.$refs.swiper.swiper.realIndex)
     }
   },
-  mounted: function() {
+  mounted: function () {
     console.log(this.$refs.swiper.swiper.realIndex)
   },
   components: {
@@ -50,9 +57,12 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .swiper-style {
   height: 500px;
   background-color: burlywood;
+}
+.swiper-slide {
+  width: 300px;
 }
 </style>
