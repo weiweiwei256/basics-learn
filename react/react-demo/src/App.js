737b1f2b6e6fc70646c476261abd3ac72ef8f243
weiyajun2688 <weiyajun2688@ipalfish.com>
2020-03-28 15:25:11 +0800

    hello world

--- react/react-demo/src/App.js
+++ react/react-demo/src/App.js
@@ -1,33 +1,11 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import Demo from './components/Demo'
-import TimeCpt from './components/TimeCpt'
-import MyToggle from './components/MyToggle'
-import MyForm from './components/MyForm'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Demo name="demo-name-1" />
-        <TimeCpt />
-        <MyToggle />
-        <MyForm />
-      </header>
-    </div>
-  )
+    return (
+        <div className="App">
+          <h1>hello world</h1> 
+        </div>
+    )
 }
 
 export default App
--- react/react-demo/src/components/Demo.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-class Demo extends React.Component {
-  render() {
-    return <h1>this is a demo components, {this.props.name}</h1>
-  }
-}
-export default Demo
