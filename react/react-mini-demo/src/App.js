d91cd3b623decf762d1a0d24fdd195d2d8d1132a
RUNNERUP <raj2011@163.com>
2020-04-19 15:59:08 +0800

    formily init

---
+++ react/react-mini-demo/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import Formily from './components/Fomily/Formily'
+function App() {
+    return (
+        <div className="App">
+            <h3>This is app header</h3>
+            <Formily></Formily>
+        </div>
+    )
+}
+export default App
