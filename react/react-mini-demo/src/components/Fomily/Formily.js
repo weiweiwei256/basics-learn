7bedbba78b51dc81c8c20aa1df9b463697a27c98
RUNNERUP <raj2011@163.com>
2020-04-19 15:59:08 +0800

    formily init

---
+++ react/react-mini-demo/src/components/Fomily/Formily.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Form, FormItem, FormButtonGroup, Reset, Submit } from "@formily/next";
+import { Input } from "@alifd/next";
+import "@alifd/next/dist/next.css"
+const Formily = () => {
+  return (
+    <div>
+      <p>This is formily</p>
+      <Form
+        onSubmit={(value) => {
+          console.log(`onSubmit value:`+JSON.stringify(value));
+        }}
+      >
+        <FormItem
+          name="name"
+          label="姓名"
+          placeholder="请填写姓名"
+          component={Input}
+        ></FormItem>
+        <FormButtonGroup>
+          <Submit></Submit>
+          <Reset></Reset>
+        </FormButtonGroup>
+      </Form>
+    </div>
+  );
+};
+export default Formily;
